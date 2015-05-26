66ff3745447d9640053d91f6a36eaa2b09485557
WangQ <wangqu.0913@gmail.com>
2015-05-27 00:16:07 +0800

    1.5.2的apktool 错误输出是standarOutput,改为标准输出和错误输出均用一个管道好了

--- apktool-gui/ViewController.m
+++ apktool-gui/ViewController.m
@@ -107,11 +107,8 @@
             
             NSPipe *pipe = [[NSPipe alloc]init];
             //恩 java的输出是标准错误输出，很科学你妹啊~
-            if (cell.tag == 0) {
-                task.standardOutput = pipe;
-            }else{
-                task.standardError = pipe;
-            }
+            task.standardOutput = pipe;
+            task.standardError = pipe;
 
             [[pipe fileHandleForReading] waitForDataInBackgroundAndNotify];
             //下面摘自 某处。。。
